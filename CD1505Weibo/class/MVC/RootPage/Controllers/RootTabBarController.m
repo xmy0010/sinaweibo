d3ab73255a85960e8bfa27754f8ead70dde498e1
xmy0010 <www.18144200589@163.com>
2015-12-30 19:02:48 +0800

    提交了第二天老师版本

--- CD1505Weibo/class/MVC/RootPage/Controllers/RootTabBarController.m
+++ CD1505Weibo/class/MVC/RootPage/Controllers/RootTabBarController.m
@@ -4,7 +4,7 @@
 //
 //  Created by HeHui on 15/12/29.
 //  Copyright (c) 2015年 Hawie. All rights reserved.
-//
+//  这是我的git
 
 #import "RootTabBarController.h"
 #import "HomeTableViewController.h"
@@ -12,6 +12,8 @@
 #import "DiscoveryTableViewController.h"
 #import "PersonalTableViewController.h"
 #import "WBTabBar.h"
+#import <PopMenu.h>
+
 
 @interface RootTabBarController () <WBTabBarDelegate>
 
@@ -20,6 +22,8 @@
 
 // @a assing @t strong  @y copy @w weak
 
+/** 弹出界面*/
+@property (nonatomic, strong) PopMenu *popMenu;
 
 @end
 
@@ -29,8 +33,7 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor orangeColor];
     
-   
-    
+
     // 添加自定义TabBar
     [self addCustomTabBar];
     
@@ -38,9 +41,35 @@
     
     
     [self addViewControlers];
-    
-   
-    
+
+}
+
+- (PopMenu *)popMenu
+{
+    if (_popMenu == nil) {
+        
+        NSMutableArray *items = [NSMutableArray array];
+        
+        NSArray *titleArr = @[@"文字",@"相册",@"微博",@"签到",@"点评",@"更多"];
+        NSArray *imgNameArr = @[@"tabbar_compose_idea",
+                                @"tabbar_compose_photo",
+                                @"tabbar_compose_weibo",
+                                @"tabbar_compose_lbs",
+                                @"tabbar_compose_review",
+                                @"tabbar_compose_more"];
+        for (int i = 0; i < titleArr.count; i++) {
+            MenuItem *item = [[MenuItem alloc] initWithTitle:titleArr[i] iconName:imgNameArr[i] glowColor:[UIColor magentaColor]];
+            [items addObject:item];
+        }
+        
+        _popMenu = [[PopMenu alloc] initWithFrame:[UIScreen mainScreen].bounds items:items];
+        
+        _popMenu.didSelectedItemCompletion = ^ (MenuItem * item) {
+            
+            NSLog(@"点击了%@",item.title);
+        };
+    }
+    return _popMenu;
 }
 
 
@@ -55,6 +84,10 @@
     __weak typeof(self) weakSelf = self;
     self.wbTabBar.passIndex = ^(NSInteger index) {
         weakSelf.selectedIndex = index;
+    };
+    self.wbTabBar.plBlock = ^ {
+       // 显示弹出界面
+        [weakSelf.popMenu showMenuAtView:weakSelf.view];
     };
     
     
